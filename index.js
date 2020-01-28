0a32012ced989d4fb369189b8495b6ab6f3973ec
Jay Jay <jayjay@Jays-MacBook-Pro.local>
2020-01-27 19:29:33 -0500

    ttransfer

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ var cors = require('cors');
 // configs come from standard PostgreSQL env vars
 // https://www.postgresql.org/docs/9.6/static/libpq-envars.html
 const pool = new pg.Pool({
-	PGHOST: process.env.PGHOST,
-	PGPORT: process.env.PGPORT,
-	PGDATABASE: process.env.PGDATABASE,
-	PGUSER: process.env.PGUSER,
-	PGPASSWORD: process.env.PGPASSWORD
+  PGHOST: process.env.PGHOST,
+  PGPORT: process.env.PGPORT,
+  PGDATABASE: process.env.PGDATABASE,
+  PGUSER: process.env.PGUSER,
+  PGPASSWORD: process.env.PGPASSWORD
 });
 //req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
@@ -33,43 +33,42 @@ app.use(bypass);
 app.use(rateLimiter);
 
 const queryHandler = (req, res, next) => {
-	//if user limit-rates are met, then this will be bypassed
-	if (!req.bypass.queryHandler) {
-		pool
-			.query(req.sqlQuery, Object.values(req.body))
-			.then(r => {
-				return res.json(r.rows || []);
-			})
-			.catch(next);
-	} else {
-		next();
-	}
+  //if user limit-rates are met, then this will be bypassed
+  if (!req.bypass.queryHandler) {
+    pool
+      .query(req.sqlQuery, Object.values(req.body))
+      .then(r => {
+        return res.json(r.rows || []);
+      })
+      .catch(next);
+  } else {
+    next();
+  }
 };
 
 app.get('/', (req, res) => {
-	res.send('Welcome to EQ Works 😎');
+  res.send('Welcome to EQ Works 😎');
 });
 
-app.post(
-	'/events/hourly',
-	(req, res, next) => {
-		req.sqlQuery = `
-    SELECT date, hour, events, poi_id
+app.get(
+  '/events/hourly',
+  (req, res, next) => {
+    req.sqlQuery = `
+    SELECT date, hour, events, public.hourly_events.poi_id
     FROM public.hourly_events
-    WHERE poi_id = $1
     ORDER BY date, hour
     LIMIT 168;
   `;
-		console.log(req.query);
-		return next();
-	},
-	queryHandler
+    console.log(req.query);
+    return next();
+  },
+  queryHandler
 );
 
 app.post(
-	'/events/daily',
-	(req, res, next) => {
-		req.sqlQuery = `
+  '/events/daily',
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT date, SUM(events) AS events, poi.name
     FROM public.hourly_events
     JOIN public.poi ON  public.hourly_events.poi_id=public.poi.poi_id
@@ -79,72 +78,72 @@ app.post(
     LIMIT 7;
   `;
 
-		return next();
-	},
-	queryHandler
-);
-
-app.post(
-	'/stats/hourly',
-	(req, res, next) => {
-		req.sqlQuery = `
-    SELECT date, hour, impressions, clicks, revenue, poi_id
-    FROM public.hourly_stats
-    WHERE poi_id = $1
-    ORDER BY date, hour
-    LIMIT 168;
-  `;
-		return next();
-	},
-	queryHandler
-);
-
-app.post(
-	'/stats/daily',
-	(req, res, next) => {
-		req.sqlQuery = `
-    SELECT date,
-        SUM(impressions) AS impressions,
-        SUM(clicks) AS clicks,
-        SUM(revenue) AS revenue
-    FROM public.hourly_stats
-    WHERE poi_id = $1
-    GROUP BY date
-    ORDER BY date
-    LIMIT 7;
-  `;
-		return next();
-	},
-	queryHandler
+    return next();
+  },
+  queryHandler
 );
 
 app.get(
-	'/poi',
-	(req, res, next) => {
-		req.sqlQuery = `
+  '/stats/hourly',
+  (req, res, next) => {
+    req.sqlQuery = `
+    SELECT date, hour, impressions, clicks, revenue, poi_id
+    FROM public.hourly_stats
+    ORDER BY date, hour
+    LIMIT 168;
+  `;
+    return next();
+  },
+  queryHandler
+);
+
+app.post(
+  '/stats/daily',
+  (req, res, next) => {
+    req.sqlQuery = `
+    SELECT date,
+        SUM(impressions) AS impressions,
+        SUM(clicks) AS clicks,
+        SUM(revenue) AS revenue, public.poi.name
+    FROM public.hourly_stats
+    JOIN public.poi ON public.hourly_stats.poi_id = public.poi.poi_id
+    WHERE public.poi.poi_id = $1
+    GROUP BY date, name
+    ORDER BY date
+    LIMIT 7;
+  `;
+    return next();
+  },
+  queryHandler
+);
+
+app.get(
+  '/poi',
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT *
     FROM public.poi;
   `;
-		return next();
-	},
-	queryHandler
+    return next();
+  },
+  queryHandler
 );
 
 app.listen(process.env.PORT || 5555, err => {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-	} else {
-		console.log(`Running on ${process.env.PORT || 5555}`);
-	}
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  } else {
+    console.log(`Running on ${process.env.PORT || 5555}`);
+  }
 });
 
 // last resorts
 process.on('uncaughtException', err => {
-	console.log(`Caught exception: ${err}`);
-	process.exit(1);
+  console.log(`Caught exception: ${err}`);
+  process.exit(1);
 });
 process.on('unhandledRejection', (reason, p) => {
-	console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
-	process.exit(1);
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  process.exit(1);
 });
